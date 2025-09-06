0ca3ffb1bb98fae13c7fcc50f614848bcc096387
dashubei <dashubei6@gmail.com>
2025-09-06 09:45:20 +0900

    update: adtro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,6 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://github.com/dashubei/my-astro-blog/",
+  site: "https://dashubei.github.io",
+  base: "my-astro-blog",
 });
